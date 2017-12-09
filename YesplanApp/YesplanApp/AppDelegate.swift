e14241b187a9d259579d02677b1f3b0f3e6e71ab
Tom Slegers <Tom@ptr-luor2956kta3ykmnow.18120a2.ip6.access.telenet.be>
2017-12-09 14:13:05 +0100

    Storyboards edit: png's added

---
+++ YesplanApp/YesplanApp/AppDelegate.swift
@@ -0,0 +1,88 @@
+//
+//  AppDelegate.swift
+//  YesplanApp
+//
+//  Created by Tom Slegers on 8/12/17.
+//  Copyright © 2017 TomPlan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        let tabBarController: UITabBarController? = self.window?.rootViewController as? UITabBarController
+        
+        if tabBarController != nil {
+            tabBarController?.delegate = self
+            if tabBarController!.viewControllers != nil {
+                for i in 0 ..< tabBarController!.viewControllers!.count {
+                    let vc: UIViewController = tabBarController!.viewControllers![i]
+                    vc.tabBarItem.tag = i
+                }
+            }
+        }
+        
+        let defaults: UserDefaults = UserDefaults.standard
+        let tabOrder: [Int]? = defaults.object(forKey: "tabOrder") as? [Int]
+        
+        if tabOrder != nil {
+            var vcOrder: [UIViewController] = []
+            for tag: Int in tabOrder! {
+                for vc in tabBarController!.viewControllers! {
+                    if vc.tabBarItem.tag == tag {
+                        vcOrder.append(vc)
+                    }
+                }
+            }
+            tabBarController?.viewControllers = vcOrder
+        }
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
+extension AppDelegate: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didEndCustomizing viewControllers: [UIViewController], changed: Bool) {
+        if changed {
+            var tabOrder: [Int] = []
+            
+            for vc: UIViewController in viewControllers {
+                tabOrder.append(vc.tabBarItem.tag)
+            }
+            
+            let defaults: UserDefaults = UserDefaults.standard
+            defaults.set(tabOrder, forKey: "tabOrder")
+        }
+    }
+}
+
