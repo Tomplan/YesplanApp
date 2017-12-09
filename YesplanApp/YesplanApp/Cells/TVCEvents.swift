52cd5a23be6c20f83d046f8faa3f701305fcca36
Tom Slegers <Tom@macbook-pro-van-techcc.local>
2017-12-09 19:41:38 +0100

    VCEvents + TVCEvents

---
+++ YesplanApp/YesplanApp/Cells/TVCEvents.swift
@@ -0,0 +1,28 @@
+//
+//  TVCEvents.swift
+//  YesplanApp
+//
+//  Created by Tom Slegers on 9/12/17.
+//  Copyright © 2017 TomPlan. All rights reserved.
+//
+
+import UIKit
+
+class TVCEvents: UITableViewCell {
+
+    @IBOutlet weak var LblEventsDefaultschedulestarttime: UILabel!
+    @IBOutlet weak var LblEventLocation: UILabel!
+    @IBOutlet weak var LblEventName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
